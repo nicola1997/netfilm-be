5cc1876e79d43bf41a0c0b03435fde287fd3bf12
ndipinto <nicola.dipinto@dxc.com>
2024-10-09 19:51:25 +0200

    Initial commit

---
+++ src/main/java/com/example/netfilmbe/NetfilmBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.netfilmbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfilmBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetfilmBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/netfilmbe/NetfilmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netfilmbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfilmBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
